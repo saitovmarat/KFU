689ba52028e25be1161db65ba4f40e66cf4f1cf2
saitovmarat <maratsajtov@gmail.com>
2022-09-14 19:54:28 +0300

    сделал

---
+++ дз задание 8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace дз_задание_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vis = 2;
+            int osn_b = 5;
+            int osn_m = 1;
+            double st = Math.Sqrt(Math.Pow((osn_b-osn_m)/2, 2) + Math.Pow(vis, 2));
+            Console.WriteLine(st);
+        }
+    }
+}
