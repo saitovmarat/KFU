30621bb32d5a825544b3d4b5c2ca4f890905fb42
saitovmarat <maratsajtov@gmail.com>
2022-09-15 18:32:29 +0300

    сделал

---
+++ дз задание 10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace дз_задание_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ дз здаание 9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace дз_здаание_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int konf = 2;
+            int pech = 3;
+            int yabl = 4;
+            Console.WriteLine("Сколько вы взяли кг конфет? ");
+            double x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Сколько вы взяли кг печенья? ");
+            double y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Сколько вы взяли кг яблок? ");
+            double z = int.Parse(Console.ReadLine());
+            Console.WriteLine($"С вас {konf * x + pech * y + yabl * z} руб");
+        }
+    }
+}
