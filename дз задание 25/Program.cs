06397e60af428ef2d7c4cecd6ce5c9e113b561f9
saitovmarat <maratsajtov@gmail.com>
2022-09-16 18:53:40 +0300

    сделал

---
+++ дз задание 25/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace дз_задание_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ дз задание 26/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace дз_задание_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double h = 8;
+            double m = 45;
+            double s = 23;
+            double overall_h = h + m / 60 + s / 3600;
+            Console.WriteLine(overall_h*30);
+        }
+    }
+}
