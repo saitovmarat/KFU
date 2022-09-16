8f4b70e38c60c126486b3c5a09584380a3116918
saitovmarat <maratsajtov@gmail.com>
2022-09-16 17:48:04 +0300

    дз 21 сделал

---
+++ дз задание 21/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace дз_задание_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(543/130);
+        }
+    }
+}
