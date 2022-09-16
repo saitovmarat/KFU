825f66cb5c354c3be61de4d4cfadf659607b3863
saitovmarat <maratsajtov@gmail.com>
2022-09-16 18:19:01 +0300

    дз задание 23 сделал

---
+++ дз задание 23/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace дз_задание_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 9768977;
+            Console.WriteLine($"число сотен  n = {(n / 100) % 10}");
+            Console.WriteLine($"число тысяч  n = {(n / 1000) % 10}");
+
+        }
+    }
+}
