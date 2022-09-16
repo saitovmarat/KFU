a301a6cedb9bfe5edb17e05356226a639b191344
saitovmarat <maratsajtov@gmail.com>
2022-09-16 17:52:54 +0300

    дз 22, сделал

---
+++ дз задание 22/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace дз_задание_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 478;
+            Console.WriteLine((num/10)+((num%10)*100));
+        }
+    }
+}
