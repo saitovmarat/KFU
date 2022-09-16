38b3c921a78ff5f2068eed2356d5bd2f83b5fa54
saitovmarat <maratsajtov@gmail.com>
2022-09-16 18:34:40 +0300

    дз задание 24 сделал

---
+++ дз задание 24/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace дз_задание_24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = "7048";
+            Console.WriteLine($"Число {num}");
+            Console.WriteLine($"а) {num[3]}{num[2]}{num[1]}{num[0]}");
+            Console.WriteLine($"б) {num[1]}{num[0]}{num[3]}{num[2]}");
+            Console.WriteLine($"в) {num[0]}{num[2]}{num[1]}{num[3]}");
+            Console.WriteLine($"г) {num[2]}{num[3]}{num[0]}{num[1]}");
+        }
+    }
+}
