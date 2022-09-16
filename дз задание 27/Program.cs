6182a9b190f5789bf55f033312fc209bd23c3b55
saitovmarat <maratsajtov@gmail.com>
2022-09-16 19:18:00 +0300

    сделал

---
+++ дз задание 27/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace дз_задание_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ дз задание 28/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace дз_задание_28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напиши число ");
+            int num1 = Math.Abs(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Напиши число ");
+            int num2 = Math.Abs(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Напиши число ");
+            int num3 = Math.Abs(int.Parse(Console.ReadLine()));
+            int min1 = Math.Min(num1, num2);
+            int min = Math.Min(min1, num3);
+            Console.WriteLine($"Минимальное набранное число = {min}");
+        }
+    }
+}
